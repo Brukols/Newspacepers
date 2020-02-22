d083c94b89fc370216dd27fb1c67c13b850abe80
Benjamin Bourgeois <benjishift@gmail.com>
2020-02-22 19:14:33 +0100

    Add news article rss

---
+++ epi_space/lib/news/NewsRssRoute.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewsRssRoute extends StatelessWidget {
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text("Article"),
+            ),
+            body: ListView(
+                children: <Widget>[],
+            )
+        );
+    }
+}
